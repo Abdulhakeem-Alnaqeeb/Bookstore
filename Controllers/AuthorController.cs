ed5ff214544cd7bb8814e833ace374c4965b3c98
Abdulhakeem <abdulhakeem.alnaqeeb.@gmail.com>
2020-07-25 12:19:42 +0300

    Add project files.

---
+++ Controllers/AuthorController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bookstore.Models;
+using Bookstore.Models.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IBookstoreRepository<Author> authorRepository;
+
+        public AuthorController(IBookstoreRepository<Author> authorRepository)
+        {
+            this.authorRepository = authorRepository;
+        }
+
+        
+        public ActionResult Index()
+        {
+            var authors = authorRepository.List();
+
+            return View(authors);
+        }
+
+       
+        public ActionResult Details(int id)
+        {
+            var author = authorRepository.Find(id);
+
+            return View(author);
+        }
+
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Author author)
+        {
+            try
+            {
+                authorRepository.Add(author);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+       
+        public ActionResult Edit(int id)
+        {
+            var author = authorRepository.Find(id);
+
+            return View(author);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Author author)
+        {
+            try
+            {
+                authorRepository.Update(id, author);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+      
+        public ActionResult Delete(int id)
+        {
+            var author = authorRepository.Find(id);
+
+            return View(author);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Author author)
+        {
+            try
+            {
+                authorRepository.Delete(id);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
